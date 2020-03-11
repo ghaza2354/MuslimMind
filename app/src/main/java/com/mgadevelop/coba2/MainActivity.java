60842be6ca217c0149b34a273d00d44f21ffc757
Ghaza Ataillah <user@muhamadeu-gaja-atilla.local>
2020-03-11 09:45:03 +0700

    Initial commit

---
+++ app/src/main/java/com/mgadevelop/coba2/MainActivity.java
@@ -0,0 +1,50 @@
+package com.mgadevelop.coba2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.VideoView;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        int waktu_loading = 7000;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent1=new Intent(MainActivity.this,MainMenuActivity.class);
+                startActivity(intent1);
+                finish();
+            }
+        }, waktu_loading);
+
+        VideoView videoView = findViewById(R.id.intro);
+        final ProgressBar loadingAnim = findViewById(R.id.anim_loading);
+
+
+        String uriPath = "android.resource://com.mgadevelop.coba2/"+R.raw.intro;
+        Uri uri = Uri.parse(uriPath);
+        videoView.setVideoURI(uri);
+        videoView.requestFocus();
+        videoView.start();
+
+       videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+           @Override
+           public void onCompletion(MediaPlayer mediaPlayer) {
+               loadingAnim.setVisibility(View.VISIBLE);
+           }
+       });
+    }
+}
